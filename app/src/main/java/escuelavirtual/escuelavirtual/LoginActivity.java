e571e614f5b766a2dd53a7eeb0a0bb98c9daeca5
cgmanoja <cgmanoja@gmail.com>
2018-06-22 02:55:06 -0300

    Estetica - Login
    - Agrego Logo y fondo al login
    - Redistribucion de elementos
    - Actualizo icono en base al nuevo logo

--- app/src/main/java/escuelavirtual/escuelavirtual/LoginActivity.java
+++ app/src/main/java/escuelavirtual/escuelavirtual/LoginActivity.java
@@ -103,7 +103,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         });
 
         mLoginFormView = findViewById(R.id.login_form);
-        mProgressView = findViewById(R.id.login_progress);
+        mProgressView = findViewById(R.id.progress_form);
     }
 
     @Override
