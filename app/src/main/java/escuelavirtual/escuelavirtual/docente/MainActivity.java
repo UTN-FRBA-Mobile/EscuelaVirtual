3d6484cae9ca5068607013a8a6ab816698667d40
EricKalinowski <eric_manutd7@hotmail.es>
2018-06-24 15:17:02 -0300

    Fixeo bug con lista de ejercicios por curso.

--- app/src/main/java/escuelavirtual/escuelavirtual/docente/MainActivity.java
+++ app/src/main/java/escuelavirtual/escuelavirtual/docente/MainActivity.java
@@ -131,6 +131,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void gotoCurso(View view){
         Intent intent = new Intent(this, CursoActivity.class);
+        if(CursoActivity.getCursoSeleccionado() != this.findCourseSelected(view))  CursoActivity.ejercicios.removeAll(CursoActivity.ejercicios);
         CursoActivity.setCursoSeleccionado(this.findCourseSelected(view));
         startActivity(intent);
     }
